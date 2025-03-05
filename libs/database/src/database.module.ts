fcdfd83ad4250d9fd264c4b6a4e03851d239bcba
young970 <yyww9@naver.com>
2025-03-06 00:09:24 +0900

    refactor: mikroORM 설정 방식 변경

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,10 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MySqlDriver } from '@mikro-orm/mysql';
 import { DatabaseService } from './database.service';
-import mikroOrmConfig from './config/mikro-orm.config';
 import { User } from './entity/user/user.entity';
-import { UserRepository } from './entity/user/user.repository';
 
 @Global()
 @Module({
@@ -13,7 +12,27 @@ import { UserRepository } from './entity/user/user.repository';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    MikroOrmModule.forRoot(mikroOrmConfig),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        driver: MySqlDriver,
+        discovery: {
+          warnWhenNoEntities: false,
+        },
+        autoLoadEntities: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        user: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        dbName: configService.get('DB_NAME'),
+        migrations: {
+          path: 'migrations',
+          pathTs: 'migrations',
+        },
+        debug: true,
+      }),
+    }),
     MikroOrmModule.forFeature([User])
   ],
   providers: [DatabaseService],
