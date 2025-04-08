1286facd1d433d8cd6b43d1e9ed7f6312af1d7a1
YoungwooKim09 <youngwoo9699@gmail.com>
2025-04-08 18:58:01 +0900

    feat: 채팅 서버 스웨거 세팅 제거(추후 설정)

--- apps/chat-server/src/main.ts
+++ apps/chat-server/src/main.ts
@@ -1,22 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { setupSwagger } from 'libs/common/src/utils/swagger';
 import { ChatServerModule } from './chat-server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ChatServerModule);
-  const configService = app.get(ConfigService);
-
-  setupSwagger(app, {
-    title: 'Chat Server',
-    description: 'Chat Server Document',
-    path: 'api-docs',
-    credentials: {
-      username: configService.get('SWAGGER_USERNAME'),
-      password: configService.get('SWAGGER_PASSWORD'),
-    },
-  });
-
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
