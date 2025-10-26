5b7748957bebb87a3a430b60c30af3fc48ee64cf
young970 <yyww9@naver.com>
2025-10-26 16:42:06 +0900

    feat: 관심지역 리스트 조회 API 응답값 추가
    admin1Code (1차 행정구역 코드) 추가

--- apps/api-server/src/user/dto/inrerest-resion-info.dto.ts
+++ apps/api-server/src/user/dto/inrerest-resion-info.dto.ts
@@ -16,16 +16,29 @@ export class InterestRegionInfoDto {
   regionName: string;
 
   @ApiProperty({
+    description: '1차 행정구역 코드',
+    required: false,
+    example: 'AL',
+  })
+  admin1Code?: string;
+
+  @ApiProperty({
     description: '기본 관심지역 여부',
     example: true,
     default: false,
   })
   isPrimary: boolean;
 
-  constructor(regionId: number, regionName: string, isPrimary: boolean) {
+  constructor(
+    regionId: number,
+    regionName: string,
+    isPrimary: boolean,
+    admin1Code?: string,
+  ) {
     this.regionId = regionId;
     this.regionName = regionName;
     this.isPrimary = isPrimary;
+    this.admin1Code = admin1Code;
   }
 }
 
@@ -47,6 +60,7 @@ export class InterestRegionInfosDto {
           interestRegion.getRegion().getId(),
           interestRegion.getRegion().getName(),
           interestRegion.getIsPrimary(),
+          interestRegion.getRegion().getAdmin1Code(),
         );
       },
     );
