b2973655a40c902eb3aa91ded60bac5303b16e9f
YoungwooKim09 <youngwoo9699@gmail.com>
2025-10-28 23:57:47 +0900

    fix: 기본 이미지 오타 수정

--- libs/common/src/constants.ts
+++ libs/common/src/constants.ts
@@ -3,7 +3,7 @@
  */
 export const USER_CONSTANTS = {
   /** 기본 프로필 이미지 S3 키 */
-  DEFAULT_PROFILE_IMAGE_KEY: 'user/1752996873456-default-profile.png',
+  DEFAULT_PROFILE_IMAGE_KEY: 'user/1752996873456-default_profile.png',
   /** 관심지역 최대 개수 */
   INTEREST_REGIONS_MAX_COUNT: 3,
 } as const;
